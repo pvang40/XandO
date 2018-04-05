5cc40276d9628bdd57bb96558906243dce25f700
pvang40 <pvang40@mail.csuchico.edu>
2018-04-04 19:55:49 -0700

    cout who the winner is

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-//Author:
+//Author: Pachia Vang 
 #include <iostream>
 
 using namespace std;
@@ -13,7 +13,9 @@ const int QUIT = -1;
 
 int main()
 {
-  //3x3 matrix of characters, initially with blank spaces in each position
+  char winner = ' ';
+  
+//3x3 matrix of characters, initially with blank spaces in each position
   char board[ROWS][COLUMNS] = {{BLANK, BLANK, BLANK},
                                {BLANK, BLANK, BLANK},
                                {BLANK, BLANK, BLANK}};
@@ -40,14 +42,7 @@ int main()
     {
       cout<<"Values must be between 0 and 2.\n";
     }
-    //TODO: Place the piece and swap turns
-    //Outline
-    //1. When neither of the (above) if nor else if statements are true, then...
-    //1-A.  Assign the board location of row and column to the value of turn
-    //1-B.  When turn has the value 'X', then...
-    //1-B-1.  turn should be assigned the value 'O'
-    //1-C.  In any other case, then...
-    //1-C-1.  turn should be assigned the value 'X'
+    
     else 
     {
         board[row][column] = turn; 
@@ -69,16 +64,27 @@ int main()
         } 
         cout<<endl;
     } 
-    //TODO: Print the current board
-    //Outline
-    //1. Traverse through each row, calling the current row r
-    //1-A. Traverse through each column in row r, calling the current column c
-    //1-A-1.  Display the value of the board at location of r and c
-    //1-A-2. Display a space
-    //1-B. Display an newline to move to the next row of the board
+
+if ((board[0][0] == 'X' && board[1][0] == 'X' && board[2][0] == 'X') || (board[0][0] == 'X' && board[0][1] == 'X' && board[0][2] == 'X' ) || ( board[1][0] == 'X'
+  && board[1][1] == 'X' &&board[1][2] == 'X' ) || ( board[2][0] == 'X' && board[2][1] == 'X'  && board[2][2] == 'X' ) ||
+    (board[0][1] == 'X'  && board[1][1]== 'X'  && board[2][1] == 'X') || (board[0][2] == 'X' && board[1][2] == 'X'  && board[2][2] == 'X') || ( board[0][0] == 'X' &&
+      board[1][1] == 'X' && board[2][2] =='X') || ( board[2][0] == 'X' && board[1][1] == 'X' && board[0][2]== 'X' ))
+        {
+            winner = 'X';
+            playing = false;
+        }
+  if ((board[0][0] == 'O' && board[1][0] == 'O' && board[2][0] == 'O') || (board[0][0] == 'O' && board[0][1] == 'O' && board[0][2] == 'O' ) || ( board[1][0] == 'O'
+    && board[1][1] == 'O' &&board[1][2] == 'O' ) || ( board[2][0] == 'O' && board[2][1] == 'O'  && board[2][2] == 'O' ) ||
+      (board[0][1] == 'O'  && board[1][1]== 'O'  && board[2][1] == 'O') || (board[0][2] == 'O' && board[1][2] == 'O'  && board[2][2] == 'O') || ( board[0][0] == 'O' &&
+        board[1][1] == 'O' && board[2][2] =='O') || ( board[2][0] == 'O' && board[1][1] == 'O' && board[0][2]== 'O' ))
+          {
+              winner = 'O';
+                  playing = false;
+          }//1-B. Display an newline to move to the next row of the board
     
   }while( playing );
-
+ 
+  cout<<"Winner is " <<winner<<endl; 
   cout<<"Goodbye!\n";
 
   return 0;
